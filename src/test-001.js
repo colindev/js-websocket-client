f94a62522ef3468b7f802dd7eaafa90a88558382
Colin <colin1124x@gmail.com>
2015-08-03 14:32:31 +0800

    整廣播回應預測時間

--- src/test-001.js
+++ src/test-001.js
@@ -73,7 +73,7 @@
     ws.listen('broadcast-ping', function(msg){
         var reply;
 
-        ws.emit(reply = 'broadcast-pong:'+((new Date).getTime() - server_diff_ms));
+        ws.emit(reply = 'broadcast-pong:'+((new Date).getTime() + server_diff_ms + data_travel_ms));
 
         echo('emit: ['+reply+']');
     });
